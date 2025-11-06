29215b67748ae6067f0103ba0ee7f6d997e22815
cinel1n <k.d.a.cinel1n@gmail.com>
2025-11-06 20:16:15 +0200

    base-auth

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'messenger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "mess_db",
+        'USER':"postgres",
+        "HOST":'localhost',
+        "PORT":5432,
+        "PASSWORD":"p0st+",
     }
 }
 
