aedbd1a5c0d29a9f736f3f82eb797a0fd2d1e449
cinel1n <k.d.a.cinel1n@gmail.com>
2026-02-21 18:22:40 +0200

    editing

--- messenger/chat/routing.py
+++ messenger/chat/routing.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('', consumers.JoinAndLeave.as_asgi()),
     path('groups/<uuid:uuid>/', consumers.GroupConsumer.as_asgi()),
 ]
