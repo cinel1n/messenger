b1fe587f9b587539e9fbe115af6c4c4572ae0ae7
cinel1n <k.d.a.cinel1n@gmail.com>
2026-04-03 00:56:25 +0200

    start tests

--- messenger/chat/tests.py
+++ messenger/chat/tests.py
@@ -1,2 +1,26 @@
 from django.test import TestCase
+from .models import Group, Message, User, GroupMemberModel
 
+
+class MessageModelTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username="testusername001", password="testuserpassword001")
+        self.user2 = User.objects.create_user(username="testusername002", password="testuserpassword002")
+
+        self.group = Group.objects.create(name="testgroup")
+        self.group.members.add(self.user1, self.user2)
+
+        self.message = Message.objects.create(author=self.user1, content="testcontent", group=self.group)
+
+    def test_create_user(self):
+        return self.assertIsInstance(self.user1, User)
+
+    def test_create_group(self):
+        return self.assertIsInstance(self.group, Group)
+    
+    def test_create_groupmember(self):
+        groupmember = len(GroupMemberModel.objects.filter(user=self.user1, group=self.group))
+        return self.assertEqual(groupmember, 1)
+
+    def test_get_absolute_url(self):
+        return self.assertFalse(self.group.get_absolute_url() is None)
--- messenger/login/models.py
+++ messenger/login/models.py
@@ -4,4 +4,3 @@ from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import User
 
-# k.d.a.geekinbox@gmail.com st0ne
