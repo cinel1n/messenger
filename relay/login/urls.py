3cac332d4e994b470ba8001cce56f62399376007
cinel1n <k.d.a.cinel1n@gmail.com>
2026-08-30 01:53:59 +0200

    password reset and recovery

--- relay/login/urls.py
+++ relay/login/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from .views import LoginUserView, RegisterUserView, logout_, ProfileView, ProfileEditView, verify, confirm_email
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('login', LoginUserView.as_view(), name='log'),
@@ -9,6 +12,38 @@ urlpatterns = [
     path('edit', ProfileEditView.as_view(), name="edit"),
     path("verify/<uuid:uuid>", verify, name="verify"),
     path("confirm-email", confirm_email, name="confirm-email"),
-    path("<str:username>", ProfileView.as_view(), name="profile"),
+    path("profile/<str:username>", ProfileView.as_view(), name="profile"),
     
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="password_reset.html",
+            email_template_name="password_reset_email.html",
+        ),
+        name="password_reset",
+    ),
+
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="password_reset_confirm.html",
+        ),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
 ]
